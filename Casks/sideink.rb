4fbc4ed04e1065746de1f379636737872b9fab05
江畔 <jiangpan@jiangpandeMac-mini.local>
2026-07-25 14:39:23 +0800

    sideink 3.3.9

--- Casks/sideink.rb
+++ Casks/sideink.rb
@@ -1,9 +1,9 @@
 cask "sideink" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.3.8"
-  sha256 arm:   "5c27ae66ea356bbbb186bc2c8253a7f295bd898c394dccf3c864c1e319546dd8",
-         intel: "b6f055246d015d1cc4cda63ea58a28dd5ab0ea517a402039b62513d09d74d38b"
+  version "3.3.9"
+  sha256 arm:   "c77371da3994f825a3581fb7e41559e84d3942a9b50a3ae60a0b71932bb30cc2",
+         intel: "a6b35b455582221290e5f9d16071c9e9f38ef8b09fda23d5f361c9d5a8967375"
 
   url "https://github.com/linleeeeee/sideink-releases/releases/download/v#{version}/SideInk-#{arch}.dmg",
       verified: "github.com/linleeeeee/sideink-releases/"
